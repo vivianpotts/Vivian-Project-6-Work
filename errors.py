12646bf3acd248c76199e3efcd9961a4093a0ba3
vivian :) <143649824+vivianpotts@users.noreply.github.com>
2025-05-12 01:06:23 -0400

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,24 @@
+## Example of inconsistencies?
+
+- I don't have any on the commit since it won't even let me commit now but here are just a few examples of some stuff that was just inconsistent or I wasn't sure on?
+
+```python
+Traceback (most recent call last):
+  File "/Users/vivianpotts/Desktop/Comp Sci 202/computer-science-202-algorithm-engineering-project-6-vivianpotts/lcsfinder/lcsfinder/benchmark.py", line 8, in <module>
+    from .generate import generate_data
+ImportError: attempted relative import with no known parent package
+```
+
+```python
+Cannot find implementation or library stub for module named "generate"
+```
+
+```
+(base) vivianpotts@Vivians-MacBook-Pro ~ % python --version
+Python 3.12.7
+```
+
+```
+(base) vivianpotts@Vivians-MacBook-Pro computer-science-202-algorithm-engineering-project-6-vivianpotts % python --version
+Python 3.11.7
+```
